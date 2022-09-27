5de7af7a3a49684c31da8df2faf4f4c54dad379d
Norman.Ablaza <namronazalba@gmail.com>
2022-09-27 10:09:45 +0800

    install bootstrap

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,10 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
 
-const environment = require('./environment')
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
 
-module.exports = environment.toWebpackConfig()
+module.exports = environment
